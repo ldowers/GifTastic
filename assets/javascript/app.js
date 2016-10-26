6a5d4a3b645511acaa198120a9aae21eb50856eb
ldowers <ldowers@cfl.rr.com>
2016-10-26 15:38:04 -0400

    Changed link to giphy API to https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ var myPage = {
 	},
 	displayMovieInfo: function() {
 		var movie = $(this).data("name");
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$('#movieView').empty();
 		
